9449b8c3b48d45ad9f388459f19a2a6630d2cd85
hemant.bhutani <hemant.bhutani@CYG410>
2019-05-28 20:29:35 +0530

    Add project files.

---
+++ TutorialApp/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TutorialApp.Controllers
+{
+    public class TestController : Controller
+    {
+        // Controller would provide User Interaction Logic
+        public string MyFirstMethod()
+        {
+            return "Welcome to Hemant's Website";
+        }
+
+            // Public would automatically make it a ACTION
+    }
+}
---
+++ TutorialApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TutorialApp.MvcApplication" Language="C#" %>
